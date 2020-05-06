22e8719f41bfe3a8c84a904bc4b72e089cadcb5b
Amin Fahrudin <foctquanh@gmail.com>
2020-05-06 10:53:31 +0700

    adding custom toast

--- app/src/main/java/com/example/donordarah/model/BaseModel.java
+++ app/src/main/java/com/example/donordarah/model/BaseModel.java
@@ -1,4 +1,24 @@
 package com.example.donordarah.model;
 
-public class BaseModel {
+import android.app.Activity;
+import android.content.Context;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import es.dmoral.toasty.Toasty;
+
+public abstract class BaseModel extends AppCompatActivity {
+
+    public static void toastSuccess(Context context, String msg) {
+        Toasty.success(context, msg, Toasty.LENGTH_SHORT, true).show();
+    }
+
+    public static void toastWarning(Context context, String msg) {
+        Toasty.warning(context, msg, 0, true).show();
+    }
+
+    public static void toastError(Context context, String msg) {
+        Toasty.error(context, msg, Toasty.LENGTH_LONG, true).show();
+    }
 }
